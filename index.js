0114167d6a6f6e96d67470e8f436f35eee6d238a
Stendhalsynd <qmflf556@naver.com>
2023-08-16 09:36:26 +0900

    dotenv 설치

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { WebClient } = require("@slack/web-api");
 const schedule = require("node-schedule");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
