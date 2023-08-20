f54f716706df7df063980437bc00b6a07dd5ca3d
Stendhalsynd <qmflf556@naver.com>
2023-08-21 00:56:35 +0900

    edit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function getWeekNumber() {
   const now = new Date();
   const startOfYear = new Date(now.getFullYear(), 0, 1);
   const days = Math.floor((now - startOfYear) / (24 * 60 * 60 * 1000)) + 1;
-  const weekNumber = Math.ceil(days / 7) - 30;
+  const weekNumber = Math.ceil(days / 7) - 31;
   return weekNumber;
 }
 
@@ -141,6 +141,6 @@ schedule.scheduleJob({ dayOfWeek: 6, hour: 22, minute: 0 }, () => {
 });
 
 // 일요일 오후 11시 59분에 초과 메세지
-schedule.scheduleJob({ dayOfWeek: 0, hour: 0, minute: 56 }, () => {
+schedule.scheduleJob({ dayOfWeek: 0, hour: 0, minute: 57 }, () => {
   monitorChannelAndAddRemindersOver();
 });
